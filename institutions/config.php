7a25b30e7461575420ba0c70cf663c169baea17e
MD ARMAN <muhammadarman187@gmail.com>
2020-10-11 05:09:38 +0530

    Update config.php

--- institutions/config.php
+++ institutions/config.php
@@ -1,16 +1,14 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER',"localhost");
-define('DB_USERNAME',"root");
-define('DB_PASSWORD',"");
-define('DB_NAME',"institutions");
 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-      
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$server = "	sql307.epizy.com";
+$username = "epiz_26938135";
+$password = "4GUtdCnckpx";
+$dbname = "epiz_26938135_institutions";
+
+$conn = mysqli_connect($server,$username,$password,$dbname);
+
+if($conn){
+    die("Connection Failed:".mysqli_connect_error());
 }
+
 ?>
